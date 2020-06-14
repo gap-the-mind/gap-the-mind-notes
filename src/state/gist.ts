12c65c195554301e2cafa4c16f8cf10e81db00a7
Matthieu Dartiguenave <matthieu@localhost.localdomain>
2020-06-14 20:04:33 +0100

    Remove Octokit

--- src/state/gist.ts
+++ src/state/gist.ts
@@ -1,5 +1,4 @@
 import { reactive, computed } from "vue"
-import { Octokit } from "https://cdn.pika.dev/@octokit/core"
 import token from "./token"
 
 export interface GistModel {
@@ -15,23 +14,13 @@ const state = reactive<GistState>({
   result: {},
 })
 
-const octokit = new Octokit(token)
-
-async function fetchGist() {
-  const response = await octokit.graphql(`query { 
-    viewer { 
-      login
-    }
-  }`)
-
-  state.result = response
-}
+async function fetchNotes() {}
 
 export function useGist() {
   return {
     gists: computed(() => state.gists),
     result: computed(() => state.result),
     addGist: () => state.gists.push({ title: "Narf" }),
-    fetchGist,
+    fetchNotes,
   }
 }
